62a19254541176866f4026e37dacb78fbc65f081
oladayo Adekoya <adekoyadayo2010@gmail.com>
2023-10-12 13:39:56 +0100

    add loading spinner

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <span className="loading loading-bars loading-lg" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,4 @@ export { default as Filters } from "./Filters";
 export { default as ProductsContainer } from "./ProductsContainer";
 export { default as PaginationContainer } from "./PaginationContainer";
 export { default as ProductList } from "./ProductList";
+export { default as Loading } from "./Loading";
