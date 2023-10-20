df0a8247d541340448b5fa53fcd789e94b32fd27
oladayo Adekoya <adekoyadayo2010@gmail.com>
2023-10-20 13:10:55 +0100

    update cartItem fucntionality

--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
 const CartItemsList = () => {
-  return <div className="text-3xl">CartItemsList</div>;
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </div>
+  );
 };
 
 export default CartItemsList;
